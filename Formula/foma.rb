55edb7baa47f74e3b5bb3c9fd8c1ca4db7f61be7
Felipe Bañados Schwerter <banadoss@ualberta.ca>
2025-10-24 14:25:25 -0600

    Added foma library

---
+++ Formula/foma.rb
@@ -0,0 +1,23 @@
+class Foma < Formula
+  desc "Finite-state compiler and C library"
+  homepage "https://github.com/mhulden/foma"
+  license "GPL-2.0-only"
+  head "https://github.com/mhulden/foma.git"
+
+  depends_on "cmake" => :build
+  uses_from_macos "flex" => :build
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "readline"
+  end
+
+  conflicts_with "freeling", because: "freeling ships its own copy of foma"
+
+  def install
+    system "cmake", "-S", "./foma", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+end
