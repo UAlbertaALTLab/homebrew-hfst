553d81c32d7178b5357e3958d47371c763f220b8
Felipe Bañados Schwerter <banadoss@ualberta.ca>
2025-10-27 11:02:37 -0600

    Added hfst-ospell

---
+++ Formula/hfst-ospell.rb
@@ -0,0 +1,20 @@
+class HfstOspell < Formula
+  desc "HFST spell checker library and command line tool"
+  homepage "https://hfst.github.io/"
+  url "https://github.com/hfst/hfst-ospell.git",
+      tag:   "v0.5.4",
+      revision: "85edd77e959df213d2e713cbd3ca0ff9a600f462"
+
+  depends_on "autoconf" => :build
+  depends_on "libarchive"  
+  uses_from_macos "libxml2"
+
+  def install
+    system "./autogen.sh"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          *std_configure_args()
+    system "make", "install"
+  end
+
+end
